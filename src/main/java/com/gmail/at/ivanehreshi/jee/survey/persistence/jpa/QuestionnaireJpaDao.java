8fbad0ad964ad6bc45c43de18807c859c855756e
ivaneh <ivanehreshi@gmail.com>
2016-11-17 17:51:03 +0200

    Add method in QuestionnaireJpaDao

--- src/main/java/com/gmail/at/ivanehreshi/jee/survey/persistence/jpa/QuestionnaireJpaDao.java
+++ src/main/java/com/gmail/at/ivanehreshi/jee/survey/persistence/jpa/QuestionnaireJpaDao.java
@@ -1,10 +1,15 @@
 package com.gmail.at.ivanehreshi.jee.survey.persistence.jpa;
 
+import com.gmail.at.ivanehreshi.jee.survey.entity.Question;
 import com.gmail.at.ivanehreshi.jee.survey.entity.Questionnaire;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import java.util.List;
 
 @Stateless
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
 public class QuestionnaireJpaDao extends JpaDao<Questionnaire, Long>{
 
     public QuestionnaireJpaDao() {
@@ -15,5 +20,11 @@ public class QuestionnaireJpaDao extends JpaDao<Questionnaire, Long>{
     public Long create(Questionnaire questionnaire) {
         super.create(questionnaire);
         return questionnaire.getId();
+    }
+
+    public List<Question> getQuestions(Long questionnaireId) {
+        List<Question> questions = read(questionnaireId).getQuestions();
+        questions.size();
+        return questions;
     }
 }
