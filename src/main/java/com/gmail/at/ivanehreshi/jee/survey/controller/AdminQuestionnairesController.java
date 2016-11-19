e96cb477d2598e5defc72494556340aa4e1529ef
ivaneh <ivanehreshi@gmail.com>
2016-11-19 17:54:32 +0200

    Use inheritance in AdminQuestionnairesController.java instead of copy-pasted code

--- src/main/java/com/gmail/at/ivanehreshi/jee/survey/controller/AdminQuestionnairesController.java
+++ src/main/java/com/gmail/at/ivanehreshi/jee/survey/controller/AdminQuestionnairesController.java
@@ -1,51 +1,28 @@
 package com.gmail.at.ivanehreshi.jee.survey.controller;
 
-import com.gmail.at.ivanehreshi.jee.survey.entity.Questionnaire;
 import com.gmail.at.ivanehreshi.jee.survey.persistence.jpa.QuestionnaireJpaDao;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import java.util.List;
 
 @ManagedBean
 @ViewScoped
-public class AdminQuestionnairesController {
+public class AdminQuestionnairesController extends UserQuestionnairesController {
     @EJB
     private QuestionnaireJpaDao questionnaireJpaDao;
 
-    private List<Questionnaire> questionnaires;
-
     @PostConstruct
     public void update() {
-        questionnaires = questionnaireJpaDao.findAll();
+        setQuestionnaires(questionnaireJpaDao.findAll());
     }
 
-    public List<Questionnaire> getQuestionnaires() {
-        return questionnaires;
+    public QuestionnaireJpaDao getQuestionnaireJpaDao() {
+        return questionnaireJpaDao;
     }
 
-    public void deleteQuestionnaire(int index) {
-        Questionnaire q = questionnaires.get(index);
-        questionnaires.remove(index);
-        questionnaireJpaDao.delete(q.getId());
+    public void setQuestionnaireJpaDao(QuestionnaireJpaDao questionnaireJpaDao) {
+        this.questionnaireJpaDao = questionnaireJpaDao;
     }
-
-    public int countFilledQuestionnaires(Questionnaire q) {
-        return questionnaireJpaDao.countFilledQuestionnaires(q.getId());
-    }
-
-    public String showLink(Questionnaire q) {
-        return "questionnaire-created?faces-redirect=true&id=" + q.getId();
-    }
-
-    public String viewAnswers(Long questionnaireId) {
-        return "answers?faces-redirect=true&q=" + questionnaireId;
-    }
-
-    public void setQuestionnaires(List<Questionnaire> questionnaires) {
-        this.questionnaires = questionnaires;
-    }
-
 }
